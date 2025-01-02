e9da12f316d5cdd0f3475ceb4669c41c9c702259
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2025-01-02 21:43:27 +0900

    インターフェースを介して社員一覧を取得

---
+++ src/main/java/EmployeeListIF.java
@@ -0,0 +1,9 @@
+package main.java;
+
+import java.util.List;
+
+public interface EmployeeListIF {
+
+    // 社員リストを取得
+    List<Employee> findAll();
+}
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,36 +2,19 @@ package main.java;
 
 import java.util.ArrayList;
 
+import main.java.EmployeeListIF;
+import main.java.EmployeeListImpl;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-        // 社員リスト作成
-        ArrayList<Employee> employeeList = new ArrayList<>();
-
-        Employee employee1 = new Employee();
-
-        employee1.setId(1);
-        employee1.setName("鈴木義雄");
-        employee1.setSex("男性");
-        employee1.setBirthYear(1984);
-        employee1.setAddress("宮城県仙台市");
-        // 社員リストに追加
-        employeeList.add(employee1);
-
-        Employee employee2 = new Employee();
-
-        employee2.setId(2);
-        employee2.setName("大島咲希");
-        employee2.setSex("女性");
-        employee2.setBirthYear(2004);
-        employee2.setAddress("福岡県福岡市");
-        // 社員リストに追加
-        employeeList.add(employee2);
+        // インターフェースを介してオブジェクトを取得
+        EmployeeListIF employeeList = new EmployeeListImpl();
 
         System.out.println("--------------------------------------------");
 
         System.out.println("ID,名前,性別,年,住所");
-        for (Employee employee : employeeList) {
+        for (Employee employee : employeeList.findAll()) {
             System.out.println(employee.getId() + "," + employee.getName() + "," + employee.getSex() + ","
                     + employee.getBirthYear() + "," + employee.getAddress());
         }
