ad568a1257fa4b0266dbe34cbc48b3eb611b98bd
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2025-01-04 19:29:57 +0900

    JSPプロジェクト作成し、http://localhost:8080/test.htmlにアクセスするとテストページが返されるコードを追加

---
+++ src/main/java/Spring04JSP/Spring04JspApplication.java
@@ -0,0 +1,12 @@
+package Spring04JSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring04JspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring04JspApplication.class, args);
+    }
+}
