23c4c8f84b2adfdb20f5b0037ad3464c4215f4d1
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2025-01-04 17:41:44 +0900

    Serviceの実装

---
+++ src/main/java/Spring03IF/service/EmployeeServiceImpl.java
@@ -0,0 +1,22 @@
+package Spring03IF.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import Spring03IF.entity.Employee;
+import Spring03IF.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Override
+    public ArrayList<Employee> findAll() {
+        return null;
+    }
+
+}
