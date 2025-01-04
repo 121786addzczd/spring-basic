07ccee763a3087354ca7af913b1b35f82e4794fa
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2025-01-04 18:43:27 +0900

    サービス層のQualifuerを用いて呼び出し先のインターフェースを変更できるようコード追加

--- src/main/java/Spring03IF/repository/EmployeeRepositoryImpl2.java
+++ src/main/java/Spring03IF/repository/EmployeeRepositoryImpl2.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Repository;
 import Spring03IF.entity.Employee;
 
 @Repository
-@Qualifier("EmployeeRepositoryImpl")
-public class EmployeeRepositoryImpl implements EmployeeRepository {
+@Qualifier("EmployeeRepositoryImpl2")
+public class EmployeeRepositoryImpl2 implements EmployeeRepository {
 
     @Override
     public ArrayList<Employee> findAll() {
@@ -19,22 +19,12 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
         Employee employee1 = new Employee();
 
         employee1.setId(1);
-        employee1.setName("鈴木義雄");
+        employee1.setName("匿名太郎");
         employee1.setSex("男性");
-        employee1.setBirthYear(1984);
-        employee1.setAddress("宮城県仙台市");
+        employee1.setBirthYear(2000);
+        employee1.setAddress("東京都立川市");
         // 社員リストに追加
         employeeList.add(employee1);
-
-        Employee employee2 = new Employee();
-
-        employee2.setId(2);
-        employee2.setName("大島咲希");
-        employee2.setSex("女性");
-        employee2.setBirthYear(2004);
-        employee2.setAddress("福岡県福岡市");
-        // 社員リストに追加
-        employeeList.add(employee2);
 
         return employeeList;
     }
--- src/main/java/Spring03IF/service/EmployeeServiceImpl.java
+++ src/main/java/Spring03IF/service/EmployeeServiceImpl.java
@@ -3,15 +3,18 @@ package Spring03IF.service;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import Spring03IF.entity.Employee;
 import Spring03IF.repository.EmployeeRepository;
+import Spring03IF.repository.EmployeeRepositoryImpl;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
+    @Qualifier("EmployeeRepositoryImpl")
     EmployeeRepository employeeRepository;
 
     @Override
